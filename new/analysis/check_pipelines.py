a8dacf68b1dbfa7af3558ae0ae8d10b0d4980185
Felix Neutatz <neutatz@gmail.com>
2022-01-14 03:56:53 +0100

    run comparison of hyperparameters

--- new/analysis/check_pipelines.py
+++ new/analysis/check_pipelines.py
@@ -1,105 +1,130 @@
 import pickle
 import numpy as np
 import pandas as pd
+import copy
 
 import argparse
 
-def dict2table(check_classifiers, count_classifiers):
+def dict2table(check_components, count_classifiers):
     table = ''
-    for check_i in range(len(check_classifiers)):
-        table += check_classifiers[check_i] + ' & '
+    for check_i in range(len(check_components)):
+        table += check_components[check_i] + ' & '
     table += '\n'
-    for check_i in range(len(check_classifiers)):
-        table += str(count_classifiers[check_classifiers[check_i]]) + ' & '
+    for check_i in range(len(check_components)):
+        table += str(count_classifiers[check_components[check_i]]) + ' & '
     print(table + '\n\n')
 
+def get_hyperparameter_values(mypath):
+    results = pickle.load(open(mypath, 'rb'))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--path')
-args = parser.parse_args()
+    check_components = ["'classifier:__choice__': 'adaboost'",
+                        "'classifier:__choice__': 'bernoulli_nb'",
+                        "'classifier:__choice__': 'decision_tree'",
+                        "'classifier:__choice__': 'extra_trees'",
+                        "'classifier:__choice__': 'gaussian_nb'",
+                        "'classifier:__choice__': 'gradient_boosting'",
+                        "'classifier:__choice__': 'k_nearest_neighbors'",
+                        "'classifier:__choice__': 'lda'",
+                        "'classifier:__choice__': 'liblinear_svc'",
+                        "'classifier:__choice__': 'libsvm_svc'",
+                        "'classifier:__choice__': 'mlp'",
+                        "'classifier:__choice__': 'multinomial_nb'",
+                        "'classifier:__choice__': 'passive_aggressive'",
+                        "'classifier:__choice__': 'qda'",
+                        "'classifier:__choice__': 'random_forest'",
+                        "'classifier:__choice__': 'sgd'",
 
-print(args.path)
+                        "'feature_preprocessor:__choice__': 'densifier'",
+                        "'feature_preprocessor:__choice__': 'extra_trees_preproc_for_classification'",
+                        "'feature_preprocessor:__choice__': 'fast_ica'",
+                        "'feature_preprocessor:__choice__': 'feature_agglomeration'",
+                        "'feature_preprocessor:__choice__': 'kernel_pca'",
+                        "'feature_preprocessor:__choice__': 'kitchen_sinks'",
+                        "'feature_preprocessor:__choice__': 'liblinear_svc_preprocessor'",
+                        "'feature_preprocessor:__choice__': 'no_preprocessing'",
+                        "'feature_preprocessor:__choice__': 'nystroem_sampler'",
+                        "'feature_preprocessor:__choice__': 'pca'",
+                        "'feature_preprocessor:__choice__': 'polynomial'",
+                        "'feature_preprocessor:__choice__': 'random_trees_embedding'",
+                        "'feature_preprocessor:__choice__': 'select_rates_classification'",
+                        "'feature_preprocessor:__choice__': 'truncatedSVD'",
 
-results = pickle.load(open(args.path, 'rb'))
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'quantile_transformer'",
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'minmax'",
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'none'",
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'normalize'",
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'power_transformer'",
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'quantile_transformer'",
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'robust_scaler'",
+                        "'data_preprocessor:feature_type:numerical_transformer:rescaling:__choice__': 'standardize'",
 
-print(results['scores'])
+                        "'data_preprocessor:feature_type:categorical_transformer:categorical_encoding:__choice__': 'encoding'",
+                        "'data_preprocessor:feature_type:categorical_transformer:categorical_encoding:__choice__': 'no_encoding'",
+                        "'data_preprocessor:feature_type:categorical_transformer:categorical_encoding:__choice__': 'one_hot_encoding'",
 
-check_classifiers = ["'classifier:__choice__': 'adaboost'",
- "'classifier:__choice__': 'bernoulli_nb'",
- "'classifier:__choice__': 'decision_tree'",
- "'classifier:__choice__': 'extra_trees'",
- "'classifier:__choice__': 'gaussian_nb'",
- "'classifier:__choice__': 'gradient_boosting'",
- "'classifier:__choice__': 'k_nearest_neighbors'",
- "'classifier:__choice__': 'lda'",
- "'classifier:__choice__': 'liblinear_svc'",
- "'classifier:__choice__': 'libsvm_svc'",
- "'classifier:__choice__': 'mlp'",
- "'classifier:__choice__': 'multinomial_nb'",
- "'classifier:__choice__': 'passive_aggressive'",
- "'classifier:__choice__': 'qda'",
- "'classifier:__choice__': 'random_forest'",
- "'classifier:__choice__': 'sgd'"]
-check_feature_processors = ["'feature_preprocessor:__choice__': 'densifier'",
- "'feature_preprocessor:__choice__': 'extra_trees_preproc_for_classification'",
- "'feature_preprocessor:__choice__': 'fast_ica'",
- "'feature_preprocessor:__choice__': 'feature_agglomeration'",
- "'feature_preprocessor:__choice__': 'kernel_pca'",
- "'feature_preprocessor:__choice__': 'kitchen_sinks'",
- "'feature_preprocessor:__choice__': 'liblinear_svc_preprocessor'",
- "'feature_preprocessor:__choice__': 'no_preprocessing'",
- "'feature_preprocessor:__choice__': 'nystroem_sampler'",
- "'feature_preprocessor:__choice__': 'pca'",
- "'feature_preprocessor:__choice__': 'polynomial'",
- "'feature_preprocessor:__choice__': 'random_trees_embedding'",
- "'feature_preprocessor:__choice__': 'select_rates_classification'",
- "'feature_preprocessor:__choice__': 'truncatedSVD'"]
+                        "'data_preprocessor:feature_type:numerical_transformer:imputation:strategy': 'most_frequent'",
+                        "'data_preprocessor:feature_type:numerical_transformer:imputation:strategy': 'mean'",
+                        "'data_preprocessor:feature_type:numerical_transformer:imputation:strategy': 'median'",
 
-count_classifiers = {}
-count_feature_processors = {}
+                        "'data_preprocessor:feature_type:categorical_transformer:category_coalescence:__choice__': 'no_coalescense'",
+                        "'data_preprocessor:feature_type:categorical_transformer:category_coalescence:__choice__': 'minority_coalescer'",
 
-for check_i in range(len(check_classifiers)):
-    count_classifiers[check_classifiers[check_i]] = 0
+                        "'balancing:strategy': 'weighting'",
+                        "'balancing:strategy': 'none'",
 
-for check_i in range(len(check_feature_processors)):
-    count_feature_processors[check_feature_processors[check_i]] = 0
+                        ]
 
-print(count_feature_processors)
+    count_classifiers = {}
 
-count_models = 0
+    for check_i in range(len(check_components)):
+        count_classifiers[check_components[check_i]] = 0
 
-for i in range(len(results['models'])):
-    print('\n\n\n')
-    auto_ml_model = results['models'][i]
-    #print(auto_ml_model.autosklearn_model.show_models())
-    all_models_with_weights = auto_ml_model.autosklearn_model.get_models_with_weights()
-    for model_i in range(len(all_models_with_weights)):
-        pipeline = auto_ml_model.autosklearn_model.get_models_with_weights()[model_i][1]
-        count_models += 1
-        print(pipeline)
-        for check_i in range(len(check_classifiers)):
-            if check_classifiers[check_i] in str(pipeline):
-                count_classifiers[check_classifiers[check_i]] += 1
-        for check_i in range(len(check_feature_processors)):
-            if check_feature_processors[check_i] in str(pipeline):
-                count_feature_processors[check_feature_processors[check_i]] += 1
+    count_models = 0
 
-print(count_classifiers)
-print(np.sum(list(count_classifiers.values())))
-print(len(auto_ml_model.autosklearn_model.get_models_with_weights()))
+    for i in range(len(results['models'])):
+        print('\n\n\n')
+        auto_ml_model = results['models'][i]
+        # print(auto_ml_model.autosklearn_model.show_models())
+        all_models_with_weights = auto_ml_model.autosklearn_model.get_models_with_weights()
+        for model_i in range(len(all_models_with_weights)):
+            pipeline = auto_ml_model.autosklearn_model.get_models_with_weights()[model_i][1]
+            count_models += 1
+            #print(pipeline)
+            for check_i in range(len(check_components)):
+                if check_components[check_i] in str(pipeline):
+                    count_classifiers[check_components[check_i]] += 1
 
-dict2table(check_classifiers, count_classifiers)
+    #print(count_classifiers)
+    #print(np.sum(list(count_classifiers.values())))
+    #print(len(auto_ml_model.autosklearn_model.get_models_with_weights()))
 
-dict2table(check_feature_processors, count_feature_processors)
+    count_components_relative = copy.deepcopy(count_classifiers)
+    for k,v in count_components_relative.items():
+        count_components_relative[k] = v / float(count_models)
+
+    return count_classifiers, check_components, count_components_relative, count_models
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path')
+    args = parser.parse_args()
+
+    print(args.path)
+
+    count_classifiers, check_components = get_hyperparameter_values(args.path)
 
 
 
-print(count_feature_processors)
-print(np.sum(list(count_feature_processors.values())))
 
-print(count_models)
 
-for i in range(len(results['feature_importances'])):
+    dict2table(check_components, count_classifiers)
+
+    '''
+    print(count_models)
+    
+    for i in range(len(results['feature_importances'])):
     print('\n\n\n')
     feature_importances = results['feature_importances'][i]
     print(feature_importances)
+    '''
---
+++ new/analysis/compare.py
@@ -0,0 +1,39 @@
+from new.analysis.check_pipelines import get_hyperparameter_values
+from new.analysis.check_pipelines import dict2table
+import copy
+
+import glob
+print(glob.glob("/home/neutatz/data/cleanml_results/*_clean.p"))
+
+my_latex_table = ''
+
+for clean_file in glob.glob("/home/neutatz/data/cleanml_results/*_clean.p"):
+    task_name = clean_file.split('/')[-1][:-11]
+    dirty_file = clean_file[:-7] + 'dirty.p'
+
+    count_classifiers_clean, check_components, count_components_relative_clean, count_models_clean = get_hyperparameter_values(clean_file)
+    count_classifiers_dirty, _, count_components_relative_dirty, count_models_dirty = get_hyperparameter_values(dirty_file)
+
+    if len(my_latex_table) == 0:
+        my_latex_table = '\\toprule\n'
+        my_latex_table += 'Task &'
+        for check_i in range(len(check_components)):
+            my_latex_table += check_components[check_i] + ' & '
+        my_latex_table = my_latex_table[:-2]
+        my_latex_table += '\\\\ \n'
+        my_latex_table += '\\midrule\n'
+
+    dict2table(check_components, count_classifiers_dirty)
+    #dict2table(check_components, count_classifiers_clean)
+
+    count_diff_rel = {}
+    for k, v in count_components_relative_clean.items():
+        count_diff_rel[k] = v - count_components_relative_dirty[k]
+
+    my_latex_table += task_name + ' &'
+    for check_i in range(len(check_components)):
+        my_latex_table += str(count_diff_rel[check_components[check_i]]) + ' & '
+    my_latex_table = my_latex_table[:-2]
+    my_latex_table += '\\\\ \n'
+
+print(my_latex_table)
