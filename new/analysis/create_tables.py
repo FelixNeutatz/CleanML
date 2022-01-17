240ef9d6182a125a470bad28000542a1915741a8
Felix Neutatz <neutatz@gmail.com>
2022-01-17 13:51:27 +0100

    plot all tables

--- new/analysis/create_tables.py
+++ new/analysis/create_tables.py
@@ -1,15 +1,23 @@
 import pickle
 import numpy as np
 
-my_latex_table = ''
+def create_table(files, title):
+    my_latex_table = ''
 
-for file_name in ['airbnb_outliers.py', 'credit_outliers.py', 'eeg_outliers.py', 'sensor_outliers.py']:
-    my_path_clean = "/home/neutatz/data/cleanml_results/" + str(file_name) + "_clean.p"
-    my_path_dirty = "/home/neutatz/data/cleanml_results/" + str(file_name) + "_dirty.p"
+    print('Outliers')
+    for file_name in ['airbnb_outliers.py', 'credit_outliers.py', 'eeg_outliers.py', 'sensor_outliers.py']:
+        my_path_clean = "/home/neutatz/data/cleanml_results/" + str(file_name) + "_clean.p"
+        my_path_dirty = "/home/neutatz/data/cleanml_results/" + str(file_name) + "_dirty.p"
 
-    result_clean = pickle.load(open(my_path_clean, 'rb'))
-    result_dirty = pickle.load(open(my_path_dirty, 'rb'))
+        result_clean = pickle.load(open(my_path_clean, 'rb'))
+        result_dirty = pickle.load(open(my_path_dirty, 'rb'))
 
-    my_latex_table += "%s & $%.2f \\pm %.2f$ & $%.2f \\pm %.2f$ \\\\ \n" % (file_name.split('.')[0], np.average(result_dirty['scores']), np.std(result_dirty['scores']), np.average(result_clean['scores']), np.std(result_clean['scores']))
+        my_latex_table += "%s & $%.2f \\pm %.2f$ & $%.2f \\pm %.2f$ \\\\ \n" % (file_name.split('.')[0], np.average(result_dirty['scores']), np.std(result_dirty['scores']), np.average(result_clean['scores']), np.std(result_clean['scores']))
 
-print(my_latex_table)
+    print(my_latex_table)
+    print('\n\n')
+
+create_table(['airbnb_outliers.py', 'credit_outliers.py', 'eeg_outliers.py', 'sensor_outliers.py'], 'outliers')
+create_table(['airbnb_missing_values.py','credit_missing_values.py','marketing_missing_values.py','titanic_missing_values.py','us_census_missing_values.py'], 'missing values')
+create_table(['company_inconsistency.py', 'movie_inconsistency.py', 'restaurant_inconsistency.py', 'university_inconsistency.py'], 'inconsistency')
+create_table(['airbnb_duplicates.py','citation_duplicates.py','movie_duplicates.py','restaurant_duplicates.py'], 'duplicates')
