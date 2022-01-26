e4134ecac89ed8f3a540a7f28d255b8df3d7f1e3
Felix Neutatz <neutatz@gmail.com>
2022-01-26 07:32:48 +0100

    fix bug 4

--- new/run_mislabels.sh
+++ new/run_mislabels.sh
@@ -1,15 +1,7 @@
-python mislabels/credit_mislabels.py -mislabel_percent 0.05 -clean_validation_labels False
-python mislabels/eeg_mislabels.py -mislabel_percent 0.05 -clean_validation_labels False
-python mislabels/us_census_mislabels.py -mislabel_percent 0.05 -clean_validation_labels False
+python mislabels/credit_mislabels.py -mislabel_percent 0.05
+python mislabels/eeg_mislabels.py -mislabel_percent 0.05
+python mislabels/us_census_mislabels.py -mislabel_percent 0.05
 
-python mislabels/credit_mislabels.py -mislabel_percent 0.1 -clean_validation_labels False
-python mislabels/eeg_mislabels.py -mislabel_percent 0.1 -clean_validation_labels False
-python mislabels/us_census_mislabels.py -mislabel_percent 0.1 -clean_validation_labels False
-
-python mislabels/credit_mislabels.py -mislabel_percent 0.05 -clean_validation_labels True
-python mislabels/eeg_mislabels.py -mislabel_percent 0.05 -clean_validation_labels True
-python mislabels/us_census_mislabels.py -mislabel_percent 0.05 -clean_validation_labels True
-
-python mislabels/credit_mislabels.py -mislabel_percent 0.1 -clean_validation_labels True
-python mislabels/eeg_mislabels.py -mislabel_percent 0.1 -clean_validation_labels True
-python mislabels/us_census_mislabels.py -mislabel_percent 0.1 -clean_validation_labels True
+python mislabels/credit_mislabels.py -mislabel_percent 0.1
+python mislabels/eeg_mislabels.py -mislabel_percent 0.1
+python mislabels/us_census_mislabels.py -mislabel_percent 0.1
